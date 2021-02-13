74aa4f73e519d8d0072e522c4a6ca4d024234a10
paultofunmio <paultofunmi01@gmail.com>
2021-02-13 15:39:11 +0100

    Added key:generate command to setup.sh"

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker exec -it lumiform_assessment-app bash -c "composer install"
+docker exec -it lumiform_assessment-app bash -c "php artisan key:generate"
 docker exec -it lumiform_assessment-app bash -c "php artisan migrate"
 docker exec -it lumiform_assessment-app bash -c "php artisan passport:install --force"
 
